0971906b47bbf21fc4ce8cf18331fe742323ee08
adamw00000 <adam.wawrzenczyk@gmail.com>
2018-10-12 21:01:10 +0200

    Initial commit

---
+++ CV 2 HR/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CV_2_HR.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
